b4bf4c7ea3e875629d3833045885a1afdfc011f5
Aitor Etxezarreta <aitoretxeza@gmail.com>
2014-10-06 22:17:19 +0200

    OperacionesCirculo

---
+++ src/OperacionesCirculo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/* Programa para calcular el area de un circulo con el radio que se introduce. */
+
+public class OperacionesCirculo {
+
+	public static void main (String args[]) {
+
+		double radiointroducido, area, circunferencia;
+
+		Circulo circulo1 = new Circulo();
+	
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("\nIntroduce el radio del circulo: ");
+
+		radiointroducido = sc.nextDouble();
+
+		circulo1.setRadio(radiointroducido);
+
+		area = circulo1.area();
+		circunferencia = circulo1.circunferencia();
+
+
+
+		System.out.println("\nEl area del circulo es: " + area + "m2");
+		System.out.println("\nLa circunferencia del circulo es: " + circunferencia + "m");
+
+	}
+}
